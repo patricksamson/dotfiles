5355eea08fdaaf6fb1a40629f71ae95284610775
Patrick Samson <Lykegenes@users.noreply.github.com>
2018-01-02 18:08:49 -0500

    Disable some apps

--- apps.sh
+++ apps.sh
@@ -11,7 +11,7 @@ brew cask install iina
 brew cask install insomnia
 brew cask install keeweb
 # brew cask install mpv
-brew cask install onyx
+# brew cask install onyx
 brew cask install otp-auth
 brew cask install rescuetime
 brew cask install safari-technology-preview # From caskroom/versions
@@ -40,20 +40,20 @@ mas install 477670270	# 2Do
 mas install 1055307502	# 2STP Companion
 mas install 918858936	# Airmail 3
 mas install 1091189122	# Bear
-mas install 1039633667	# Irvue
+# mas install 1039633667	# Irvue
 mas install 402989379	# iStudiez Pro
 mas install 409183694	# Keynote
 mas install 926036361	# LastPass
-mas install 1039598025	# Lockdown
+# mas install 1039598025	# Lockdown
 mas install 715768417	# Microsoft Remote Desktop
 mas install 409203825	# Numbers
 mas install 409201541	# Pages
 mas install 803453959	# Slack
-mas install 1278508951 	# Trello
+# mas install 1278508951 	# Trello
 mas install 1284863847	# Unsplash Wallpapers
 mas install 461369673	# Vox
-mas install 1000397973	# Wallcat
-mas install 410628904	# Wunderlist
+# mas install 1000397973	# Wallcat
+# mas install 410628904	# Wunderlist
 # mas install 497799835	# Xcode
 
 # Cleanup
