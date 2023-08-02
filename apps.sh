68b45450f73ff9d5040915f0e26ee4bd3d3778e9
Patrick Samson <patricksamson236@gmail.com>
2023-08-01 20:27:54 -0400

    Add more apps

--- apps.sh
+++ apps.sh
@@ -27,6 +27,7 @@ brew install microsoft-teams
 brew install ngrok
 # brew install openemu
 brew install openscad
+brew install orbstack
 brew install orion
 brew install postman
 brew install prusaslicer
@@ -60,6 +61,7 @@ brew install mas
 # Print the Apple account currently in use, or log into one?
 mas account
 
+mas install 1365531024		# 1Blocker
 mas install 1569813296		# 1Password for Safari
 mas install 477670270		# 2Do
 mas install 1440147259		# AdGuard for Safari
@@ -69,8 +71,9 @@ mas install 1364502317      # Clockify
 mas install 1438243180 		# Dark Reader for Safari
 mas install 290986013       # Deliveries
 mas install 975937182       # Fantastical
+mas install 1195676848		# Grocery
 mas install 1544743900		# Hush
-# mas install 1039633667	# Irvue
+mas install 1136220934		# Infuse
 mas install 409183694		# Keynote
 mas install 926036361       # LastPass
 mas install 1295203466		# Microsoft Remote Desktop
@@ -79,13 +82,13 @@ mas install 409201541		# Pages
 mas install 639968404		# Parcel
 mas install 1515445551		# Pi-hole Remote
 mas install 1459275972 		# Playlisty
+mas install 1578144015		# Privacy Redirect
 mas install 803453959		# Slack
 mas install 1509611686      # Spaces
 mas install 747648890		# Telegram
 mas install 425424353		# The Unarchiver
 mas install 1284863847		# Unsplash Wallpapers
-# mas install 461369673		# Vox
-# mas install 1000397973	# Wallcat
+mas install 1451685025		# WireGuard
 # mas install 497799835		# Xcode
 
 
